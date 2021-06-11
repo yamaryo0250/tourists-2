10dc3a07fff383e535c75336e4b17a204d20e979
yamaryo0250 <ryo.yamagiwa0250@gmail.com>
2021-06-11 09:26:04 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+  validates :nickname
+  validates :birthday
+  end
 end
