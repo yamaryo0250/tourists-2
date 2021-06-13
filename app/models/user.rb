1b94239415c43cc6a7d2115fe0ab6b67837d0e02
yamaryo0250 <ryo.yamagiwa0250@gmail.com>
2021-06-13 19:15:50 +0900

    投稿とユーザーの依存関係

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :tourings
+  has_many :tourings, dependent: :destroy
 
   with_options presence: true do
   validates :nickname
