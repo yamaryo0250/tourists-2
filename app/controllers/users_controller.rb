f822e3d8fc11c3f34043821fd7ad7669ca3958fb
yamaryo0250 <ryo.yamagiwa0250@gmail.com>
2021-06-22 21:52:04 +0900

    コントローラーの実装と、新規登録ビューの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @tourings = @user.tourings
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   resources :tourings do
     resources :likes, only: [:create, :destroy]
   end
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :edit]
 end
